26db783bc5a9f2ffd2b0551c6ea55c2ee7dbdad1
radio <radio.github@gmail.com>
2014-03-23 22:43:57 -0700

    Fixes album art sizes on hover, and shortens button transitions

--- public/assets/application.js
+++ public/assets/application.js
@@ -1,3 +1,1 @@
-// require bootstrap
-// normal stuff
-jQuery(function(e){"use strict";e("#get_now ul").on("click","input",function(){this.select()})});
+jQuery(function(t){"use strict";t("#get_now ul").on("click","input",function(){this.select()})});
